a1f115eedd9d33450e1beafcfbcd85d2a5d5fae5
Abdullah All Abir <akumaabir@Abdullahs-Mac-mini.local>
2024-05-26 22:52:06 +0600

    initialized project

---
+++ LocateMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocateMe
+//
+//  Created by Abdullah All Abir on 26/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocateMe/LocateMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocateMeApp.swift
+//  LocateMe
+//
+//  Created by Abdullah All Abir on 26/5/24.
+//
+
+import SwiftUI
+
+@main
+struct LocateMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
